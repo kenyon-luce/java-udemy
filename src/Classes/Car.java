7d4d6085cdedb3c74434262cb2637b8e6dba8d77
Kenyon Luce <kenyon.luce02@gmail.com>
2021-10-12 12:34:26 -0500

    created classes

---
+++ src/Classes/Car.java
@@ -0,0 +1,13 @@
+package Classes;
+
+//public --unrestricted access to method
+public class Car {
+
+    // creating private fields
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+    //
+}
---
+++ src/Classes/Main.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class Main {
+    //CLASSES --templates for creating Java objects ^^
+    public static void main(String[] args){
+        //creating local variable, calling on object
+        Car nissan = new Car();
+        Car toyota = new Car();
+
+        // nissan.model = "Altima";
+    }
+}
